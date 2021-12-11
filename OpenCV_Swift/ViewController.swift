7ce8584c85ccf06c218fa6f189471b6fb8e83ed4
Hanagasaki <stze961q@gmail.com>
2021-12-12 02:33:32 +0900

    PickViewを追加しました。

--- OpenCV_Swift/ViewController.swift
+++ OpenCV_Swift/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var grayScaleImage: UIImageView!
     
-    let bool: Bool = true
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let imageArray = ["GrayScale", "Canny", "Sobel", "Laplacian", "Gaussian", "Add"]
+    var rowNum: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +32,8 @@ class ViewController: UIViewController {
         let tapped = UITapGestureRecognizer(target: self, action: #selector(self.tappedGetImage(_:)))
         tapped.delegate = self
         grayScaleImage.addGestureRecognizer(tapped)
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
     
     @objc private func tappedProfileImageButton() {
@@ -71,16 +76,10 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         profileImageButton.contentVerticalAlignment = .fill
         profileImageButton.clipsToBounds = true
         
-        let image = profileImageButton.imageView?.image
+        guard let image = profileImageButton.imageView?.image else { return }
         
-        grayScaleImage.image = OpenCVSample.dilate(image!)
+        grayScaleImage.image = imageProccess(rowNum: rowNum, image: image)
             
-        //OpenCVSample.add(image!)
-        //OpenCVSample.gaussianBlur(image!)
-        //OpenCVSample.laplacian(image!)
-        //OpenCVSample.sobel(image!)
-        //OpenCVSample.canny(image!)
-        //OpenCVSample.grayScale(image!)
         grayScaleImage.contentMode = .scaleAspectFill
         grayScaleImage.clipsToBounds = true
         
@@ -108,4 +107,43 @@ extension ViewController: UIGestureRecognizerDelegate {
             // UIAlertController を表示
             self.present(alert, animated: true, completion: nil)
         }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.imageArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        rowNum = row
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageArray[row]
+    }
+    
+    func imageProccess(rowNum: Int, image: UIImage) -> UIImage {
+        switch rowNum {
+        case 0:
+            return OpenCVSample.grayScale(image)
+        case 1:
+            return OpenCVSample.canny(image)
+        case 2:
+            return OpenCVSample.sobel(image)
+        case 3:
+            return OpenCVSample.laplacian(image)
+        case 4:
+            return OpenCVSample.gaussianBlur(image)
+        case 5:
+            return OpenCVSample.add(image)
+        default:
+            break
+        }
+        return (profileImageButton.imageView?.image)!
+    }
+    
 }
